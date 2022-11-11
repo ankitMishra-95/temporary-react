afaf06a7cd2dcf7af17d14c1fd9564e6cb391bed
Ankit Mishra <ankit.mishra@jungleegames.com>
2022-11-11 17:09:09 +0530

    test

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section className='section about-section'>
       <h1 className='section-title'>about page</h1>
-      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution</p>
+      <p>It is a long established fact that a readerr will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution</p>
     </section>
   )
 }
